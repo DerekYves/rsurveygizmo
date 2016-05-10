300359363b1a5b140cca763a8d552c75ad3fc005
Derek Darves <ddarves@cpg.org>
2016-05-10 13:41:16 -0400

    New options added: save geo variable

--- R/pullsg.R
+++ R/pullsg.R
@@ -1,25 +1,27 @@
 #' Pull the raw survey response data from Survey Gizmo, storing an unmodified dataframe.
 #'
 #' This function downloads a survey's response data from Survey Gizmo (SG), saving
-#' the returned data as a \code{\link{data.frame}}. Because SG limits the size of JSON data pulls via the API (currently, the limit is 250),
+#' the returned data as an R \code{\link{data.frame}}. Because SG limits the size of JSON data pulls via the API (currently, the limit is 250),
 #' it calculates the number of pulls needed to download the entire response set and binds the returned frames.
 #' To ensure that variable names are interpretable in the returned data frame, it is strongly recommended
 #' that users first assign question \href{https://help.surveygizmo.com/help/article/link/using-question-aliases}{aliases} to each question prior to utilizing this function.
 #'
-#' @param sg_surveyid The survey's unique SG ID number (in V4 of the API, the portion of the \href{https://apihelp.surveygizmo.com/help/article/link/surveyresponse-sub-object}{surveyresponse}call URL which follows "id/", e.g.: "...build/id/1234567"
+#' @param sg_surveyid The survey's unique SG ID number (in V4 of the API, the portion of the \href{https://apihelp.surveygizmo.com/help/article/link/surveyresponse-sub-object}{surveyresponse} call URL which follows "id/", e.g.: "...build/id/1234567"
 #' @param api The user's private API key for Survey Gizmo
-#' @param completes Should the download drop partial responses (Default)?
-#' @param delete_sys_vars Delete all SG system variables (i.e., non-response fields of the form [[variable(...)]] from the returned data.frame (see \href{https://apihelp.surveygizmo.com/help/article/link/surveyresponse-returned-fields}{this link} for documentation)?
-#' @param reset_row_names Reset row.names to 1, 2,..N in the returned dataframe (Default)?
+#' @param completes_only When true (the Default), survey responses with a status of "Complete" are saved. Responses with a status of "disqualified", "partial", etc. are deleted (see \href{https://apihelp.surveygizmo.com/help/article/link/surveyresponse-returned-fields}{this link} for documentation)
+#' @param delete_sys_vars When true, deletes SG system variables (i.e., fields of the form [[variable(...)]] that do \emph{not} contain question responses. See \href{https://apihelp.surveygizmo.com/help/article/link/surveyresponse-returned-fields}{this link} for more information about fields returned by SG via the API)?
+#' @param keep_geo_vars When true (the default), SG's geographic variables (lat/long, city, etc.), which are estimated using the respondent's IP address, are preserved in the returned data.frame
+#' @param reset_row_names When true (the default), resets row names to 1, 2,..N in the returned dataframe
 #' @param clean This option performs three transformations to the returned data.frame:
 #' \enumerate{
 #'   \item attempts to coerce vectors to numeric if all values are numbers or "" (uses \code{\link{type.convert}})
-#'   \item deletes SG system variables (i.e., non-question responses)
+#'   \item sets 'delete_sys_vars' to true
 #'   \item removes other non-survey-question variables returned by the Survey Gizmo API, including: "contactid", "istestdata", "sessionid", "language", "ilinkid", and "sresponsecomment" (as of V4 of the SG API)
 #' }
 #' @importFrom jsonlite fromJSON
 #' @export
-pullsg <- function(sg_surveyid, api, completes=T, delete_sys_vars=F, clean=F, reset_row_names=T) {
+pullsg <- function(sg_surveyid, api, completes=T, delete_sys_vars=F, keep_geo_vars=T, clean=F, reset_row_names=T) {
+
 	options(stringsAsFactors=F)
 
 	#Set hard-coded parameters
@@ -31,11 +33,13 @@ pullsg <- function(sg_surveyid, api, completes=T, delete_sys_vars=F, clean=F, re
 	results  <- "&resultsperpage=100"
 
 	#Build local parameters
-	surveyid <- sg_surveyid
 	filturl  <- paste0("&filter[field][0]=status&filter[operator][0]==",
 					   "&filter[value][0]=Complete")
-	if (completes==T) filt=filturl
-	else filt <- ""
+	if (completes == T) {
+		filt = filturl
+	} else {
+		filt <- ""
+	}
 
 	lc_base  <- paste0(url, surveyid, response, token, filt)
 	lc_furl  <- paste0(url, surveyid, response, token, results, filt, pages)
@@ -50,14 +54,21 @@ pullsg <- function(sg_surveyid, api, completes=T, delete_sys_vars=F, clean=F, re
 
 	# Retrieve the question list from the "/surveyquestion/" call
 	lc_qs   <- fromJSON(txt=lc_qurl)
+
 	lc_qs   <- as.data.frame(lc_qs$data)
 
-	# Recode question names that do not have an alias defined
-	lc_qs$shortname   <- ifelse(is.na(lc_qs$shortname),
-								lc_qs$title$English, lc_qs$shortname)
+	# Extract question text for questions without a defined SG alias, stripping
+	# html tags, punctuation, and spaces,  keeping the first 35 characters and
+	# appending the question id.
+	lc_qs$qtext <- gsub("<.*?>", "", lc_qs$title$English)
+	lc_qs$qtext <- gsub("[[:punct:]]", "", lc_qs$qtext)
+	lc_qs$qtext <- paste0(substr(gsub("[[:space:]]", ".", lc_qs$qtext), 1, 35),
+						  "_ID", lc_qs$id)
+	lc_qs$shortname <- ifelse(is.na(lc_qs$shortname) | lc_qs$shortname=="",
+							  lc_qs$qtext, lc_qs$shortname)
 
-	# Subset the frame, keeping shortname and id
-	lc_qs <- lc_qs[, c('id', 'shortname')]
+	# Drop instructional messages and subset the frame, keeping shortname and id.
+	lc_qs <- lc_qs[lc_qs$`_type` !="SurveyDecorative", c('id', 'shortname')]
 
 	# Retrieve the response data with the "/surveyresponse/" call
 	for(i in 1:lc_respnum){
@@ -70,44 +81,54 @@ pullsg <- function(sg_surveyid, api, completes=T, delete_sys_vars=F, clean=F, re
 	}
 
 	#Bind the returned frames
-	lc_tobind  <- ls(pattern="lc_survey_page")
-	lc_fullset <- do.call("rbind", mget(lc_tobind))
+	lc_fullset <- do.call("rbind", mget(ls(pattern="lc_survey_page")))
 
-	# Process the column names
+	# Extract the column names of the responsedata returned from SG
 	lc_names <- names(lc_fullset)
 
-	# Defined patterns to drop
+	# Define patterns to drop from the vector of column names
 	patterns = "[[:punct:]]|[[:space:]]|question|shown|STANDARD"
 
 	# Hidden values generally take form of: 'question(76), option(0)'
-	# If hidden value assigned to more than one ET field, then
-	# ...option(1)... option(N)... format is used. Password actions
-	# also have a qid with two option fields with five digit numbers
-	# Here, we extract the "option 0" parameter from questions so
-	# they will merge correctly with the array of names.
+	# If a hidden value is assigned to more than one field (e.g., in
+	# an Exact Target Pull action, then an '...option(1)... option(N)...'
+	# format is used. SG password actions also have a qid with two option
+	# fields. Here, we extract the "option 0" parameter from the questions
+	# so they will merge correctly with the array of names.
 	lc_names <- lapply(lc_names, gsub, patt= ", option(0)",
 					   rep="", fixed=T)
 
 	# Now remove the patterns saved above.
-	lc_names <- lapply(lc_names, gsub, patt= patterns,
-					   rep="")
+	lc_names <- lapply(lc_names, gsub, patt= patterns, rep="")
+
+	# Save geographiv variables based on respondent IP
+	keep_geo_vars <- T
+	if(keep_geo_vars) {
+		lc_names <- lapply(lc_names, gsub, patt="^variableLONG$", rep="rsp_lng")
+		lc_names <- lapply(lc_names, gsub, patt="^variableLAT$", rep="rsp_lat")
+		lc_names <- lapply(lc_names, gsub, patt="^variableGEOCOUNTRY$", rep="rsp_cntry")
+		lc_names <- lapply(lc_names, gsub, patt="^variableGEOCITY$", rep="rsp_city")
+		lc_names <- lapply(lc_names, gsub, patt="^variableGEOREGION$", rep="rsp_region")
+		lc_names <- lapply(lc_names, gsub, patt="^variableGEOPOSTAL$", rep="rsp_post")
+	}
+
+
 	# Now rename SG system fields, subtituting "sys_" for "variable"
 	lc_names <- lapply(lc_names, gsub, patt= "variable",
 					   rep="sys_")
-	# Now lower case the names
-	lc_names  <- as.data.frame(tolower(lc_names))
 
-	colnames(lc_names) <- "id"
+	# Now lower case the names and convert to data.frame
+	lc_names <- data.frame(id=tolower(lc_names))
 
-	# Index to preserve inital sort of question names
+	# Index to preserve the inital column sorting
 	lc_names$index <- seq_len(nrow(lc_names))
 
 	# Left join of the cleaned name table
-	lc_names <- merge(lc_names, lc_qs, all.x=T)
+	lc_names <- merge(lc_names, lc_qs, by="id", all.x=T)
 	lc_names <- lc_names[order(lc_names$index),]
 	rownames(lc_names) <- NULL # reset the row.names attribute
 
-	# Recode hidden values, which do not accept shortnames/aliases in SG
+	# Replace variable name with shortname wher available
 	lc_names$id   <- ifelse(is.na(lc_names$shortname),
 							lc_names$id, lc_names$shortname)
 
@@ -123,7 +144,7 @@ pullsg <- function(sg_surveyid, api, completes=T, delete_sys_vars=F, clean=F, re
 	if(delete_sys_vars | clean) set <- set[, -grep("^sys_", names(set),
 												   ignore.case=T)]
 
-	# Other variables to drop per the "clean" parameter (V3 API)
+	# Other variables to drop per the "clean" parameter (V4 API)
 	otherdrops <- paste0("contactid|istestdata|sessionid|",
 						 "language|ilinkid|sresponsecomment")
 
@@ -132,8 +153,16 @@ pullsg <- function(sg_surveyid, api, completes=T, delete_sys_vars=F, clean=F, re
 	if(reset_row_names) row.names(set) <- NULL
 
 	# Format Survey Gizmo date fields
-	set[, c('datestarted', 'datesubmitted')] <- lapply(set[, c('datestarted', 'datesubmitted')],
+
+	if(save_as_factor){
+		lapply(set[, c('datestarted', 'datesubmitted')],
+			   as.character)
+		set[, c('datestarted', 'datesubmitted')] <- lapply(set[, c('datestarted', 'datesubmitted')],
 								   as.POSIXct, format="%Y-%m-%d")
+	} else {
+		set[, c('datestarted', 'datesubmitted')] <- lapply(set[, c('datestarted', 'datesubmitted')],
+														   as.POSIXct, format="%Y-%m-%d")
+	}
 
 	return(set)
 }
